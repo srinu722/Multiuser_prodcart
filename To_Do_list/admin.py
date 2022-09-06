9ef8b76b50c55257a54cf6c7ea30b641c0b134ad
srinu722 <srinivasu.urimiti@gmail.com>
2022-09-06 09:14:49 +0530

    to_do list admin change

--- To_Do_list/admin.py
+++ To_Do_list/admin.py
@@ -1,19 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-from user_auth.models import User
-from To_Do_list.models import Products
-
-# Register your models here.
-class UserAdmin(admin.ModelAdmin):
-     list_display=['username','email','phone_no','first_name','last_name','password1','password2']
-
-admin.site.register(User,UserAdmin)
-
-
-
-# Register your models here.
-class ProductsAdmin(admin.ModelAdmin):
-     list_display=['name']
-
-admin.site.register(Products,ProductsAdmin)
